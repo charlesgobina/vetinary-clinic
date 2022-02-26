83f824dd5fd45919d3e6f2834e8a6cc76aee5a4d
charlesgobina <charles.gobina@gmail.com>
2022-02-26 10:24:14 +0100

    Add all listed tables

--- schema.sql
+++ schema.sql
@@ -45,3 +45,41 @@ ADD COLUMN owner_id INT,
 ADD CONSTRAINT fk_owners
 	FOREIGN KEY(owner_id)
 		REFERENCES owners(id);
+
+/*Vets table*/
+CREATE TABLE vets (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(250),
+	age INT,
+	date_of_graduation DATE,
+	PRIMARY KEY(id)
+);
+
+/*Specialisation table*/
+CREATE TABLE specializations (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	vets_id INT,
+	species_id iNT,
+	CONSTRAINT fk_vets
+		FOREIGN KEY(vets_id)
+			REFERENCES vets(id),
+	CONSTRAINT fk_species
+		FOREIGN KEY(species_id)
+			REFERENCES species(id),
+	PRIMARY KEY(id)
+);
+
+/*Visits*/
+CREATE TABLE visits (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	animals_id INT,
+	vets_id iNT,
+	visit_date DATE,
+	CONSTRAINT fk_animals
+		FOREIGN KEY(animals_id)
+			REFERENCES animals(id),
+	CONSTRAINT fk_vets
+		FOREIGN KEY(vets_id)
+			REFERENCES vets(id),
+	PRIMARY KEY(id)
+);
