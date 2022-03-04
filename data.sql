3591ef68e2aa762f6c02397431cc282c76629fdc
charlesgobina <charles.gobina@gmail.com>
2022-03-04 21:52:33 +0100

    Add new insert queries

--- data.sql
+++ data.sql
@@ -233,4 +233,11 @@ INSERT INTO visits (animals_id, vets_id, visit_date) VALUES
     ('Jan 11, 2021')
 );
 
+INSERT INTO owners (full_name, email) 
+SELECT 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+INSERT INTO visits (animals_id, vets_id, visit_date) 
+SELECT * FROM (SELECT id FROM animals) animal_ids, 
+(SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
 
